123efc6d944143fc6812888c7b20ace2f6608b1b
Tabish Raza <tabishraza@gmail.com>
2017-12-19 16:53:16 +0000

    render player

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Player from './App';
+import Api from './Api';
 import registerServiceWorker from './registerServiceWorker';
 
 
-
+//
 ReactDOM.render(<Player name1='Marco' hp1={100} action1="fuck off" name2='Cristhian' hp2={100} action2="sing"/>, document.getElementById('root'));
 registerServiceWorker();
+// ReactDOM.render(<Api />, document.getElementById('root'));
+// registerServiceWorker();
