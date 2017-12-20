3b81fc93e8f666692982109d5c659c7af694f33c
Tabish Raza <tabishraza@gmail.com>
2017-12-20 11:14:14 +0000

    refactor

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -4,9 +4,9 @@ import './Player.css'
 const Player = (props) => {
   return(
     <div className='playerSheet'>
-    <p onClick={props.click} >{props.name}</p>
-    <p> {props.hp} </p>
-    <p> {props.strength} </p>
+      <p onClick={props.click} >{props.name}</p>
+      <p> {props.hp} </p>
+      <p> {props.strength} </p>
     </div>
   )
 }
