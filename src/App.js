8d476ac5eab6f99a415d6d24018d427cec524d4b
Tabish Raza <tabishraza@gmail.com>
2017-12-20 11:13:39 +0000

    render battle component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import Player from './Player/Player'
+import Battle from './Battle'
 
 class App extends Component {
- state = {
+  constructor(props){
+    super(props);
+    this.state = {
       FirstPlayer: [],
-      SecondPlayer: []
+      SecondPlayer: [],
+      showComponent: false
     }
+    this._onButtonClick= this._onButtonClick.bind(this);
+  }
 
   togglePlayerHandler = (player) => {
     if (this.state.FirstPlayer instanceof Array){
-      console.log(player)
       this.setState({FirstPlayer: player})}
     else if (this.state.SecondPlayer instanceof Array){
-      console.log(player)
       this.setState({SecondPlayer: player})
     }
   }
 
   componentDidMount() {
     fetch('http://localhost:4000/api/players/players.json')
-      .then(d => d.json())
-      .then(d => {
+      .then(data => data.json())
+      .then(data=> {
         this.setState({
-          playerData: d
+          playerData: data
         })
       })
+  }
+
+  _onButtonClick(){
+    this.setState({
+      showComponent: true,
+    });
   }
 
   render() {
@@ -33,26 +43,22 @@ class App extends Component {
       return <p>Loading Players...</p>
 
     } else {
+        const listOfPlayers = this.state.playerData.map( (player,index) => {
+          return <Player name= {player.name}
+            click={() => this.togglePlayerHandler(player)}
+            key={index}/>;
+        });
 
-      const listOfPlayers = this.state.playerData.map( (player) => {
-        return <Player name= {player.name}
-          click={() => this.togglePlayerHandler(player)}
-          key={player.id}/>;
-      });
+      const PlayersChosen = [this.state.FirstPlayer, this.state.SecondPlayer];
 
-      const PlayersChosen = [this.state.FirstPlayer, this.state.SecondPlayer]
-
-      const SelectedPlayers = PlayersChosen.map( player => {
+      const SelectedPlayers = PlayersChosen.map( (player, index) => {
         if(this.state.FirstPlayer instanceof Array) { return <Player name= {"Select Player"}/> } else {
-        return <Player name= {player.name}
-        hp =  {"Health: " + player.hp}
-        strength = {"Strength: " + player.strength}
-        key={player.strength}/>;
-      }
+          return <Player name= {player.name}
+          hp =  {"Health: " + player.hp}
+          strength = {"Strength: " + player.strength}
+          key={index}/>;
+        }
       });
-
-
-
 
       return (
         <ul>
@@ -60,6 +66,9 @@ class App extends Component {
            {this.state.FirstPlayer.name + " Vs " + this.state.SecondPlayer.name}
 
           {SelectedPlayers}
+
+          <button onClick={this._onButtonClick}>Fight</button>
+            {this.state.showComponent ? <Battle  /> : null}
         </ul>
       );
     }
