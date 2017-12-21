5600df0b20aaf9d4296f1938f5d394956c4417ee
cristhiandas <cristhiandf94@gmail.com>
2017-12-21 15:20:42 +0000

    added random images when people attack

--- src/Battle.js
+++ src/Battle.js
@@ -21,6 +21,15 @@ class Battle extends Component {
     this.player1SecondaryAttack = this.player1SecondaryAttack.bind(this);
     this.player2SecondaryAttack = this.player2SecondaryAttack.bind(this);
   }
+  componentDidMount() {
+    fetch('http://localhost:4000/api/action_pictures/action_pictures.json')
+      .then(data => data.json())
+      .then(data=> {
+        this.setState({
+          picturesData: data
+        })
+      })
+  }
 
   randomAttack1(strength , crit){
     if((Math.random()* 100) < crit){
@@ -42,14 +51,14 @@ class Battle extends Component {
     if (this.state.turn === 1) {
       this.setState({hp2: this.state.hp2 - this.randomAttack1(this.state.strength1, this.state.crit1)});
       this.state.turn = 2;
-      this.handleAdd("http://localhost:4000" + this.props.player1.image.url)
+      this.handleAdd()
     }
   }
   player1SecondaryAttack() {
     if (this.state.turn === 1) {
      this.setState({hp2: this.state.hp2 - this.randomAttack2(this.state.strength1, this.state.crit1)});
      this.state.turn = 2;
-     this.handleAdd("http://localhost:4000" + this.props.player1.image.url)
+     this.handleAdd()
    }
   }
 
@@ -57,52 +66,56 @@ class Battle extends Component {
     if (this.state.turn === 2) {
      this.setState({hp1: this.state.hp1 - this.randomAttack1(this.state.strength2, this.state.crit2)});
      this.state.turn = 1;
-     this.handleAdd("http://localhost:4000" + this.props.player2.image.url)
+     this.handleAdd()
    }
   }
   player2SecondaryAttack() {
     if (this.state.turn === 2) {
      this.setState({hp1: this.state.hp1 - this.randomAttack2(this.state.strength2, this.state.crit2)});
      this.state.turn = 1;
-     this.handleAdd("http://localhost:4000" + this.props.player2.image.url)
+     this.handleAdd()
    }
   }
 
-  handleAdd(url) {
-    const newItems = [url]
+  handleAdd() {
+    const newItems = [this.state.picturesData[Math.floor(Math.random() * this.state.picturesData.length)]];
+    console.log(newItems)
     this.setState({items: newItems});
   }
 
   render() {
+    if(!this.state.picturesData){
+      return <p>Loading Battle...</p>
+    } else {
+      console.log(this.state.items)
+      const items = this.state.items.map((item) => (
+        <img src={"http://localhost:4000/" + item.image} key={item.id} height="100px" width="100px"/>
+      ));
 
-    const items = this.state.items.map((item, i) => (
-      <img src={item} key={item} height="100px" width="100px"/>
-    ));
-    console.log(items)
-
-    return (
-      <span>
-        <div id='player1'>
-          <p>{this.props.player1.name}</p>
-          <p>Hit Points: {this.state.hp1}</p>
-          <img src={"http://localhost:4000" + this.props.player1.image.url} height="100px" width="100px" />
-          <button onClick={this.player1PrimaryAttack}>{this.props.player1.actions[0].name}</button>
-          <button onClick={this.player1SecondaryAttack}>{this.props.player1.actions[1].name}</button>
-        </div>
-        <br />
-            {items}
-        <br />
-        <div id='player2'>
-          <p>{this.props.player2.name}</p>
-          <p>Hit Points: {this.state.hp2}</p>
-          <img src={"http://localhost:4000" + this.props.player2.image.url} height="100px" width="100px"/>
-          <button onClick={this.player2PrimaryAttack}>{this.props.player2.actions[0].name}</button>
-          <button onClick={this.player2SecondaryAttack}>{this.props.player2.actions[1].name}</button>
-        </div>
+      return (
+        <span>
+          <div id='player1'>
+            <p>{this.props.player1.name}</p>
+            <p>Hit Points: {this.state.hp1}</p>
+            <img src={"http://localhost:4000" + this.props.player1.image.url} height="100px" width="100px" />
+            <button onClick={this.player1PrimaryAttack}>{this.props.player1.actions[0].name}</button>
+            <button onClick={this.player1SecondaryAttack}>{this.props.player1.actions[1].name}</button>
+          </div>
+          <br />
+              {items}
+          <br />
+          <div id='player2'>
+            <p>{this.props.player2.name}</p>
+            <p>Hit Points: {this.state.hp2}</p>
+            <img src={"http://localhost:4000" + this.props.player2.image.url} height="100px" width="100px"/>
+            <button onClick={this.player2PrimaryAttack}>{this.props.player2.actions[0].name}</button>
+            <button onClick={this.player2SecondaryAttack}>{this.props.player2.actions[1].name}</button>
+          </div>
 
 
-      </span>
-    );
-}
+        </span>
+      );
+    }
+  }
 }
 export default Battle;
