db14f230fa3d753eaa80b28e2f40ac4cc6c2fa6a
Tabish Raza <tabishraza@gmail.com>
2017-12-21 11:09:32 +0000

    extra lines

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -3,8 +3,8 @@ import './Player.css'
 
 const Player = (props) => {
   return(
-    <div className='playerSheet'>
-    <p onClick={props.click} >{props.name}</p>
+    <div className='playerSheet' onClick={props.click}>
+    <p>{props.name}</p>
     <p> {props.hp} </p>
     <p> {props.strength} </p>
     <img style={{width: 60, height: 60}} src={props.image}></img>
