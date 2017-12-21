0312124e516a9ab430c6a1d489fde5bde0bb4630
thatdania@gmail.com <thatdania@gmail.com>
2017-12-21 17:41:59 +0000

    WIP on CSS

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -7,7 +7,7 @@ const Player = (props) => {
     <p>{props.name}</p>
     <p> {props.hp} </p>
     <p> {props.strength} </p>
-    <img style={{width: 60, height: 60}} src={props.image}></img>
+    <img style={{width: 80, height: 80}} class="image" src={props.image}></img>
     </div>
   )
 }
