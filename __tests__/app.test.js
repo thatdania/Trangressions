b714d8447685ec8e698aac0494d342ce8bcdb2fe
Lewis <lewisyoul@gmail.com>
2017-12-21 15:43:26 +0000

    first passing feature test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from '../src/App';
+import App from '../src/App';
 // import {concatStrings} from '../src/App.js';
-import { mount } from 'enzyme'
-import { shallow } from 'enzyme'
+import ReactTestUtils from 'react-dom/test-utils';
 import '../src/setupTests.js'
 import { concatStrings } from '../src/App'
+import renderer from 'react-test-renderer';
 
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
@@ -13,11 +13,12 @@ describe('Addition', () => {
   });
 });
 
-describe('String Concatenation', () => {
-  it('concatenates two strings', () => {
-    // console.log(<App />)
-    // const wrapper = mount(<App />);
-    // const inst = wrapper.instance();
-    expect(concatStrings("hi", "Oleg")).toEqual("hi Oleg");
+describe('FEATURE - App', () => {
+  it('it renders Loading Players... to the page', () => {
+    const rendered = renderer.create(
+      <App />
+    );
+    console.log(rendered.toJSON())
+    expect(rendered.toJSON()).toMatchSnapshot();
   });
 });
