e0228ab5233d3a0f9d4588807a3b97585743d502
Lewis <lewisyoul@gmail.com>
2017-12-22 14:13:23 +0000

    remove old lines of code before merging to master

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,33 +1,39 @@
 import React from 'react'
 import './Player.css'
 
+class Player extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+    this.show = this.show.bind(this)
+    this.hide = this.hide.bind(this)
+  }
 
+  show() {
+    this.setState(
+      { testState: true }
+    );
+  }
 
+  hide() {
+    this.setState(
+      { testState: false }
+    )
+  }
 
+  render(){
+    console.log(this.props)
+    return (
 
-
-const Player = props => {
-
-const hover =
-  (
-
-  <p>{props.name}</p>
-
-  )
-
-  return(
-
-    <div className='playerSheet' onClick={props.click}>
-    <p>{props.name}</p>
-    <p> {props.hp} </p>
-    <p> {props.strength} </p>
-    <img style={{width: 60, height: 60}} src={props.image} title={hover} />
-    <img style={{width: 100, height: 100}} class="image" src={props.image}></img>
-
-
-    </div>
-
-  )
+      <div className='playerSheet' onClick={this.props.click} onMouseOver={this.show} onMouseOut={this.hide}>
+      <p>{this.props.name}</p>
+      <img style={{width: 60, height: 60}} src={this.props.image} />
+      <img style={{width: 100, height: 100}} class="image" src={this.props.image}></img>
+      { this.state.testState ? <p>Health: {this.props.hp} <br></br> Strength: {this.props.strength}</p> : null }
+      </div>
+    );
+  }
 }
 
 export default Player;
